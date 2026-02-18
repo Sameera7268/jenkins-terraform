ecba0ef943208792e36c93fe4b700077d418ad2d
Sameera7268 <145865856+Sameera7268@users.noreply.github.com>
2026-02-18 14:50:11 +0530

    Update eks.tf

--- eks-terraform/eks.tf
+++ eks-terraform/eks.tf
@@ -1,18 +1,13 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
-  cluster_name = local.name  
+  version = "21.15.1"
+ name = local.name  
   subnet_ids = module.vpc.private_subnets
   enable_irsa = true
 
   vpc_id = module.vpc.vpc_id
 
-    eks_managed_node_group_defaults = {
-        ami_type ="AL2_x86_64"
-        instance_types = ["t3.medium"]
-        vpc_security_group_ids =[aws_security_group.sg.id]
-
-    }
+  
  eks_managed_node_groups = {
 
     node_group = {
